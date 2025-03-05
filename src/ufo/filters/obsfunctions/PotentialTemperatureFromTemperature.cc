antoniosandroni/ufosigma

commit 4185f31a5dea5292299997bcaa366b2451c77083

Feature/potential temperature in guess (#3573)
* Calculate potential temperature in GeoVaLs at surface or near a pressure level.

* Replaced 'geovals pressure' by 'approximate pressure'.

* Added an example to derive potential temperature at surface.

* Code norm.

* Use new GeoVaLs name "air_pressure_at_surface".

* Added "air_potential_temperature" as a GeoVaLs. Set "use surface pressure" as a resquired variable and set pressure_level as an optional.

* Updated the example in PotentialTemperatureGuess2D.h

* Code norm.

* Added a new ctest "ufo_test_tier1_test_ufo_amsua_allsky_gfs_gsi_qc" to validate configurations of AMSUA-A N19 all-sky DA with precipitable hydrometeors.

* Removed the commit of "amsua_allsky_gfs_gsi_qc.yaml" in the develop BR.

* Remove the ctest accidentally commited in the develop BR.

* Renamed the function as "PotentialTemperatureFromTemperature".

* Renamed the function "PotentialTemperatureGuess2D" as "PotentialTemperatureFromTemperature".

src/ufo/filters/obsfunctions/PotentialTemperatureFromTemperature.cc
@@ -1,0 +1,95 @@
+/*
+ * (C) Copyright 2025 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "ufo/filters/obsfunctions/PotentialTemperatureFromTemperature.h"
+
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "ioda/ObsDataVector.h"
+#include "ufo/filters/ObsFilterData.h"
+#include "ufo/filters/Variable.h"
+#include "ufo/utils/Constants.h"
+
+namespace ufo {
+
+static ObsFunctionMaker<PotentialTemperatureFromTemperature> makerPotentialTemperatureFromTemperature_
+                   ("PotentialTemperatureFromTemperature");
+
+PotentialTemperatureFromTemperature::PotentialTemperatureFromTemperature(const eckit::LocalConfiguration & conf)
+  : invars_() {
+  // Initialize options
+  options_.deserialize(conf);
+  std::ostringstream errString;
+  if (options_.useSurfacePressure.value() == false) {
+    ASSERT(options_.geovalsPressure.value() != boost::none);
+  }
+
+  if (options_.useSurfacePressure.value()) {
+     invars_ += Variable("GeoVaLs/air_pressure_at_surface");
+     invars_ += Variable("GeoVaLs/average_surface_temperature_within_field_of_view");
+  } else {
+     invars_ += Variable("GeoVaLs/air_pressure");
+     invars_ += Variable("GeoVaLs/air_potential_temperature");
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+PotentialTemperatureFromTemperature::~PotentialTemperatureFromTemperature() {}
+
+// -----------------------------------------------------------------------------
+
+void PotentialTemperatureFromTemperature::compute(const ObsFilterData & in,
+                                    ioda::ObsDataVector<float> & out) const {
+  // Get dimension
+  const size_t nlocs = in.nlocs();
+  std::vector<float> potentialTemperature(nlocs, 0.0);
+  std::vector<float> pressure(nlocs, 1.0E+5);
+  std::vector<float> temperature(nlocs, 0.0);
+
+  if (options_.useSurfacePressure.value()) {
+     in.get(Variable("GeoVaLs/air_pressure_at_surface"), pressure);
+     in.get(Variable("GeoVaLs/average_surface_temperature_within_field_of_view"), temperature);
+     // Calculate potential temperature
+     for (size_t iloc = 0; iloc < nlocs; ++iloc) {
+        potentialTemperature[iloc] =  temperature[iloc] * pow(Constants::pref / pressure[iloc],
+                     Constants::rd_over_cp);
+     }
+  } else {
+     float pressure_value = options_.geovalsPressure.value().get();
+     const size_t nlevs = in.nlevs(Variable("GeoVaLs/air_pressure"));
+     std::vector<float> dprs_min(nlocs, 9999.0);
+     std::vector<float> pre_levl(nlocs);
+     std::vector<float> air_potential_temperature(nlocs);
+     for (size_t ilev = 0; ilev < nlevs; ++ilev) {
+        in.get(Variable("GeoVaLs/air_pressure"), ilev, pre_levl);
+        in.get(Variable("GeoVaLs/air_potential_temperature"), ilev, air_potential_temperature);
+        for (size_t iloc = 0; iloc < nlocs; ++iloc) {
+           float pressure_diff = std::fabs(pre_levl[iloc] - pressure_value);
+           if (pressure_diff < dprs_min[iloc]) {
+              dprs_min[iloc] = pressure_diff;
+              potentialTemperature[iloc] = air_potential_temperature[iloc];
+           }
+        }
+     }
+  }
+  out[0] = potentialTemperature;
+}
+
+// -----------------------------------------------------------------------------
+
+const ufo::Variables & PotentialTemperatureFromTemperature::requiredVariables() const {
+  return invars_;
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo

src/ufo/filters/CMakeLists.txt
@@ -295,6 +295,8 @@ set ( filters_files
       obsfunctions/ObsErrorFactorSfcPressure.h
       obsfunctions/ObsErrorFactorConventional.cc
       obsfunctions/ObsErrorFactorConventional.h
+      obsfunctions/PotentialTemperatureFromTemperature.cc
+      obsfunctions/PotentialTemperatureFromTemperature.h
       obsfunctions/NearSSTRetCheckIR.cc
       obsfunctions/NearSSTRetCheckIR.h
       obsfunctions/ObsFunctionElementMultiply.cc

src/ufo/filters/obsfunctions/PotentialTemperatureFromTemperature.h
@@ -1,0 +1,72 @@
+/*
+ * (C) Copyright 2021 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#ifndef UFO_FILTERS_OBSFUNCTIONS_POTENTIALTEMPERATUREFROMTEMPERATURE_H_
+#define UFO_FILTERS_OBSFUNCTIONS_POTENTIALTEMPERATUREFROMTEMPERATURE_H_
+
+#include <string>
+#include "oops/util/parameters/OptionalParameter.h"
+#include "oops/util/parameters/Parameter.h"
+#include "oops/util/parameters/Parameters.h"
+#include "oops/util/parameters/RequiredParameter.h"
+#include "ufo/filters/obsfunctions/ObsFunctionBase.h"
+#include "ufo/filters/Variables.h"
+
+namespace ufo {
+
+class ObsFilterData;
+
+///
+/// \brief Calculate model potential temperature near a pressure level (Pa) or at surface.
+///  Here is an exampe to derive potential temperature at surface:
+///  - filter: Variable Assignment
+///    assignments:
+///    - name: DerivedMetaData/PotentialTemperatureSurface
+///      type: float
+///      function:
+///        name: PotentialTemperatureFromTemperature@ObsFunction
+///        options:
+///          use surface pressure: true
+///  Here is another exampe to derive potential temperature at model levels near 70000 Pa:
+///  - filter: Variable Assignment
+///    assignments:
+///    - name: DerivedMetaData/PotentialTemperatureSurface
+///      type: float
+///      function:
+///        name: PotentialTemperatureFromTemperature@ObsFunction
+///        options:
+///          use surface pressure: false
+///          pressure to evaluate potential temperature: 70000
+///
+class PotentialTemperatureFromTemperatureParameters : public oops::Parameters {
+  OOPS_CONCRETE_PARAMETERS(PotentialTemperatureFromTemperatureParameters, Parameters);
+
+ public:
+  oops::RequiredParameter<bool> useSurfacePressure{"use surface pressure", this};
+  oops::OptionalParameter<float> geovalsPressure{"pressure to evaluate potential temperature",
+                                 this};
+};
+
+class PotentialTemperatureFromTemperature : public ObsFunctionBase<float> {
+ public:
+  explicit PotentialTemperatureFromTemperature(const eckit::LocalConfiguration &
+                                       = eckit::LocalConfiguration());
+  ~PotentialTemperatureFromTemperature();
+
+  void compute(const ObsFilterData &,
+               ioda::ObsDataVector<float> &) const;
+  const ufo::Variables & requiredVariables() const;
+ private:
+  ufo::Variables invars_;
+  PotentialTemperatureFromTemperatureParameters options_;
+};
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
+
+#endif  // UFO_FILTERS_OBSFUNCTIONS_POTENTIALTEMPERATUREFROMTEMPERATURE_H_

commit 4e77e4b13fdc01a595df110a33c4e6306acb1fd5

Fix coding_norms broken by #3573 (#3632)
Lines were too long.

src/ufo/filters/obsfunctions/PotentialTemperatureFromTemperature.cc
@@ -20,11 +20,12 @@
 
 namespace ufo {
 
-static ObsFunctionMaker<PotentialTemperatureFromTemperature> makerPotentialTemperatureFromTemperature_
-                   ("PotentialTemperatureFromTemperature");
+static ObsFunctionMaker<PotentialTemperatureFromTemperature>
+  makerPotentialTemperatureFromTemperature_("PotentialTemperatureFromTemperature");
 
-PotentialTemperatureFromTemperature::PotentialTemperatureFromTemperature(const eckit::LocalConfiguration & conf)
-  : invars_() {
+PotentialTemperatureFromTemperature::PotentialTemperatureFromTemperature(
+  const eckit::LocalConfiguration & conf
+): invars_() {
   // Initialize options
   options_.deserialize(conf);
   std::ostringstream errString;