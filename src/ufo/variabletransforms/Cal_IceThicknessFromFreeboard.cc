antoniosandroni/ufosigma

commit ddfdeeeb2e1fc77b55c5dbcf2d7ba492019a33e5

Feature: ice thickness from ice freeboard variable transform (#3756)
* First draft

* Add a test configuration.

* Remove print filter data from test configuration

* Fix for Random STD calculation

* fix naming

* improve variable naming

* Add test for the total error.

* swap if logic

* Add missing opening bracket.

* Response to ctgh review.

* Test default settings and improve log messages

---------

Co-authored-by: Evan Parker <eparker@ucar.edu>
Co-authored-by: Rajeswari Chidambaram <rchidamb@ucar.edu>
Co-authored-by: Benjamin Ruston <93737224+BenjaminRuston@users.noreply.github.com>

src/ufo/variabletransforms/Cal_IceThicknessFromFreeboard.cc
@@ -1,0 +1,204 @@
+/*
+ * (C) Crown copyright 2025, Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "ufo/variabletransforms/Cal_IceThicknessFromFreeboard.h"
+
+#include <cmath>
+
+#include "eckit/log/CodeLocation.h"
+#include "ioda/ObsDataVector.h"
+#include "oops/util/Logger.h"
+#include "oops/util/missingValues.h"
+#include "ufo/filters/ObsFilterData.h"
+#include "ufo/utils/Constants.h"
+
+namespace ufo {
+
+static TransformMaker<Cal_IceThicknessFromFreeboard>
+    makerCal_IceThicknessFromFreeboard_(
+        "Calculate iceThickness from seaIceFreeboard");
+
+Cal_IceThicknessFromFreeboard::Cal_IceThicknessFromFreeboard(
+    const Parameters_ &options, const ObsFilterData &data,
+    const std::shared_ptr<ioda::ObsDataVector<int>> &flags,
+    const std::shared_ptr<ioda::ObsDataVector<float>> &obserr)
+    : TransformBase(options, data, flags, obserr), options_(options) {
+  for (const Variable &var :
+       {options.IceFreeboardVariable.value(),
+        options.WaterDensityVariable.value(), options.SnowDepthVariable.value(),
+        options.SnowDensityVariable.value(),
+        options.IceDensityVariable.value()}) {
+    if (!obsdb_.has(var.group(), var.variable())) {
+      throw eckit::BadValue(
+          "" + classname_ + ":: `" + var.fullName() +
+              "` must be a variable for this variable transform.",
+          Here());
+    }
+  }
+  if (options.calculateErrors.value()) {
+    for (const Variable &var : {options.IceFreeboardESDVariable.value(),
+                                options.IceDensityESDVariable.value(),
+                                options.SnowDensityESDVariable.value(),
+                                options.SnowDepthESDVariable.value()}) {
+      if (!obsdb_.has(var.group(), var.variable())) {
+        throw eckit::BadValue("" + classname_ + ":: `" + var.fullName() +
+                                  "` must be a variable for "
+                                  "this variable transform if calculating errors.",
+                              Here());
+      }
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+void Cal_IceThicknessFromFreeboard::runTransform(
+    const std::vector<bool> &apply) {
+  oops::Log::trace() << classname_ << "::runTransform: Calculate Ocean Ice Thickness and errors from ice "
+                        "freeboard, snow, and density observations "
+                     << std::endl;
+
+  // dimension
+  const size_t nlocs = obsdb_.nlocs();
+
+  // Set default ObsErrorData and QCflagsData information
+
+  const size_t iErrIceThickness =
+      obserr_.varnames().find(options_.IceThicknessVariable.value().variable());
+  const size_t iFlagIceThickness =
+      flags_.varnames().find(options_.IceThicknessVariable.value().variable());
+  for (size_t jobs = 0; jobs < nlocs; ++jobs) {
+    if (!apply[jobs]) continue;
+    obserr_[iErrIceThickness][jobs] = missingValueFloat;
+    flags_[iFlagIceThickness][jobs] = 0;
+  }
+
+  // compute ice thickness
+  std::vector<float> iceFreeboard;
+  std::vector<float> waterDensity;
+  std::vector<float> snowDepth;
+  std::vector<float> snowDensity;
+  std::vector<float> iceDensity;
+
+  getObservation(options_.IceFreeboardVariable.value().group(),
+                 options_.IceFreeboardVariable.value().variable(), iceFreeboard,
+                 true);
+  getObservation(options_.WaterDensityVariable.value().group(),
+                 options_.WaterDensityVariable.value().variable(), waterDensity,
+                 true);
+  getObservation(options_.SnowDepthVariable.value().group(),
+                 options_.SnowDepthVariable.value().variable(), snowDepth,
+                 true);
+  getObservation(options_.SnowDensityVariable.value().group(),
+                 options_.SnowDensityVariable.value().variable(), snowDensity,
+                 true);
+  getObservation(options_.IceDensityVariable.value().group(),
+                 options_.IceDensityVariable.value().variable(), iceDensity,
+                 true);
+
+  std::vector<float> iceThickness(nlocs, missingValueFloat);
+  for (size_t loc = 0; loc < nlocs; ++loc) {
+    if (!apply[loc]) continue;
+    if (iceFreeboard[loc] != missingValueFloat &&
+        snowDepth[loc] != missingValueFloat &&
+        snowDensity[loc] != missingValueFloat &&
+        waterDensity[loc] != missingValueFloat &&
+        iceDensity[loc] != missingValueFloat) {
+      // fi * rhow + ds * rhos) / (rhow - rhoi)
+      const float densityDifference = waterDensity[loc] - iceDensity[loc];
+      if (densityDifference < std::numeric_limits<float>::min()) {
+        iceThickness[loc] = missingValueFloat;
+      } else {
+        iceThickness[loc] = (iceFreeboard[loc] * waterDensity[loc] +
+                              snowDepth[loc] * snowDensity[loc]) /
+                            densityDifference;
+      }
+    }
+  }
+  putObservation(options_.IceThicknessVariable.value().variable(),
+                  iceThickness, options_.IceThicknessVariable.value().group());
+
+  if (!options_.calculateErrors.value()) {
+    oops::Log::trace() << classname_ << "::runTransform: done." << std::endl;
+    return;
+  }
+
+  std::vector<float> iceFreeboardESD;
+  std::vector<float> iceDensityESD;
+  std::vector<float> snowDensityESD;
+  std::vector<float> snowDepthESD;
+
+  getObservation(options_.IceFreeboardESDVariable.value().group(),
+                 options_.IceFreeboardESDVariable.value().variable(),
+                 iceFreeboardESD, true);
+  getObservation(options_.IceDensityESDVariable.value().group(),
+                 options_.IceDensityESDVariable.value().variable(),
+                 iceDensityESD, true);
+  getObservation(options_.SnowDensityESDVariable.value().group(),
+                 options_.SnowDensityESDVariable.value().variable(),
+                 snowDensityESD, true);
+  getObservation(options_.SnowDepthESDVariable.value().group(),
+                 options_.SnowDepthESDVariable.value().variable(), snowDepthESD,
+                 true);
+
+  std::vector<float> iceThicknessSystematicESD(nlocs, missingValueFloat);
+  std::vector<float> iceThicknessRandomESD(nlocs, missingValueFloat);
+  for (size_t loc = 0; loc < nlocs; ++loc) {
+    if (!apply[loc]) continue;
+    if (iceFreeboard[loc] != missingValueFloat &&
+        snowDepth[loc] != missingValueFloat &&
+        snowDensity[loc] != missingValueFloat &&
+        waterDensity[loc] != missingValueFloat &&
+        iceDensity[loc] != missingValueFloat &&
+        iceFreeboardESD[loc] != missingValueFloat &&
+        iceDensityESD[loc] != missingValueFloat &&
+        snowDensityESD[loc] != missingValueFloat) {
+      const float densityDifference = waterDensity[loc] - iceDensity[loc];
+      float iceThicknessRandomVariance = missingValueFloat;
+      float iceThicknessSystematicVariance = missingValueFloat;
+      if (densityDifference > std::numeric_limits<float>::min()) {
+        // random error
+        // sqrt((fi_std * rhow / (rhow - rhoi))**2 +
+        //       (rhoi_std * (fi * rhow + ds * rhos) /
+        //        (rhow - rhoi)**2)**2)
+        iceThicknessRandomVariance =
+            std::pow(
+                iceFreeboardESD[loc] * waterDensity[loc] / densityDifference,
+                2) +
+            std::pow(iceDensityESD[loc] *
+                         (iceFreeboard[loc] * waterDensity[loc] +
+                          snowDepth[loc] * snowDensity[loc]) /
+                         (densityDifference * densityDifference),
+                     2);
+        iceThicknessRandomESD[loc] = std::sqrt(iceThicknessRandomVariance);
+        // systematic error
+        // sqrt((ds_std * rhos / (rhow - rhoi))**2 +
+        //       (rhos_std * ds / (rhow - rhoi))**2)
+        iceThicknessSystematicVariance =
+            std::pow(snowDepthESD[loc] * snowDensity[loc] / densityDifference,
+                     2) +
+            std::pow(snowDensityESD[loc] * snowDepth[loc] / densityDifference,
+                     2);
+        iceThicknessSystematicESD[loc] =
+            std::sqrt(iceThicknessSystematicVariance);
+        obserr_[iErrIceThickness][loc] = std::sqrt(
+            iceThicknessSystematicVariance + iceThicknessRandomVariance);
+      }
+    }
+  }
+  putObservation(options_.IceThicknessRandomESDVariable.value().variable(),
+                 iceThicknessRandomESD,
+                 options_.IceThicknessRandomESDVariable.value().group());
+  putObservation(options_.IceThicknessSystematicESDVariable.value().variable(),
+                 iceThicknessSystematicESD,
+                 options_.IceThicknessSystematicESDVariable.value().group());
+  oops::Log::trace() << classname_ << "::runTransform: done." << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo

src/ufo/variabletransforms/CMakeLists.txt
@@ -12,6 +12,8 @@ set ( variabletransforms_files
       Cal_HeightFromPressure.cc
       Cal_Humidity.h
       Cal_Humidity.cc
+      Cal_IceThicknessFromFreeboard.h
+      Cal_IceThicknessFromFreeboard.cc
       Cal_Logarithm.h
       Cal_Logarithm.cc
       Cal_PotentialTFromT.h

src/ufo/variabletransforms/Cal_IceThicknessFromFreeboard.h
@@ -1,0 +1,152 @@
+/*
+ * (C) Crown copyright 2025, Met Office
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#ifndef UFO_VARIABLETRANSFORMS_CAL_ICETHICKNESSFROMFREEBOARD_H_
+#define UFO_VARIABLETRANSFORMS_CAL_ICETHICKNESSFROMFREEBOARD_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "oops/util/parameters/Parameters.h"
+
+#include "ufo/filters/Variable.h"
+#include "ufo/filters/Variables.h"
+#include "ufo/utils/parameters/ParameterTraitsVariable.h"
+#include "ufo/variabletransforms/TransformBase.h"
+
+namespace ufo {
+
+class ObsFilterData;
+
+/// \brief Options controlling Cal_IceThicknessFromFreeboard variable transform
+class Cal_IceThicknessFromFreeboardParameters
+    : public VariableTransformParametersBase {
+  OOPS_CONCRETE_PARAMETERS(Cal_IceThicknessFromFreeboardParameters,
+                           VariableTransformParametersBase)
+
+ public:
+  /// Input ice Freeboard
+  oops::Parameter<Variable> IceFreeboardVariable{
+      "ice freeboard variable", Variable("ObsValue/seaIceFreeboard"), this};
+  /// Input sea water density
+  oops::Parameter<Variable> WaterDensityVariable{
+      "sea water density variable", Variable("ObsValue/seaWaterDensity"), this};
+  /// Input snow depth
+  oops::Parameter<Variable> SnowDepthVariable{
+      "snow depth variable", Variable("ObsValue/totalSnowDepth"), this};
+  /// Input snow density
+  oops::Parameter<Variable> SnowDensityVariable{
+      "snow density variable", Variable("ObsValue/snowDensity"), this};
+  /// Input ice density
+  oops::Parameter<Variable> IceDensityVariable{
+      "ice density variable", Variable("ObsValue/iceDensity"), this};
+  /// Output ice thickness variable name
+  oops::Parameter<Variable> IceThicknessVariable{
+      "ice thickness variable", Variable("iceThickness"), this};
+  /// Output ice thickness error standard deviations
+  oops::Parameter<bool> calculateErrors{"calculate error standard deviations",
+                                        true, this};
+  /// Input sea ice freeboard error standard deviation
+  oops::Parameter<Variable> IceFreeboardESDVariable{
+      "ice freeboard error standard deviation variable",
+      Variable("ObservedErrorStandardDeviation/seaIceFreeboard"), this};
+  /// Input ice density error standard deviation
+  oops::Parameter<Variable> IceDensityESDVariable{
+      "ice density error standard deviation variable",
+      Variable("ObservedErrorStandardDeviation/iceDensity"), this};
+  /// Input snow density error standard deviation
+  oops::Parameter<Variable> SnowDensityESDVariable{
+      "snow density error standard deviation variable",
+      Variable("ObservedErrorStandardDeviation/snowDensity"), this};
+  /// Input snow depth error standard deviation
+  oops::Parameter<Variable> SnowDepthESDVariable{
+      "snow depth error standard deviation variable",
+      Variable("ObservedErrorStandardDeviation/totalSnowDepth"), this};
+  /// Output ice thickness systematic error standard deviation
+  oops::Parameter<Variable> IceThicknessSystematicESDVariable{
+      "ice thickness systematic error standard deviation variable",
+      Variable("SystematicErrorStandardDeviation/iceThickness"), this};
+  /// Output ice thickness random error standard deviation
+  oops::Parameter<Variable> IceThicknessRandomESDVariable{
+      "ice thickness random error standard deviation variable",
+      Variable("RandomErrorStandardDeviation/iceThickness"), this};
+};
+
+// -----------------------------------------------------------------------------
+
+/// \brief Calculate ice thickness from ice freeboard, ice depth, and the
+/// densities of the snow, ice, and surface water.
+/// \details The ice thickness is calculated using the formula from
+/// https://tc.copernicus.org/articles/8/1607/2014/tc-8-1607-2014.html:
+///  ice thickness = (freeboard * water density + snow depth * snow density) /
+///                  (water density - ice density)
+///  ice thickness random esd = sqrt(
+///    ( freeboard esd  * water density / (water density - ice density) )**2 +
+///    ( ice density esd * (freeboard * water density + snow depth * snow
+///    density) /
+///      (water density - ice density)**2
+///    )**2
+///  )
+///  ice thickness systematic esd = sqrt(
+///    ( snow depth esd * snow density / (water density - ice density) )**2 +
+///    ( snow density esd * snow depth / (water density - ice density) )**2
+///  )
+///
+/// This will return ice thickness (m) in a variable named (by default)
+/// "DerivedObsValue/iceThickness", given the following input variables:
+/// - ice freeboard (m)
+/// - water density (kg/m^3)
+/// - snow depth (m)
+/// - snow density (kg/m^3)
+/// - ice density (kg/m^3)
+/// The systematic and random errors of ice thickness are also calculated via
+/// gaussian error propagation.
+///
+/// Example yaml:
+///
+/// \code{.yaml}
+/// obs filters:
+/// - filter: Variable Transforms
+///   Transform: Calculate iceThickness from seaIceFreeboard
+///   ice freeboard variable: ObsValue/seaIceFreeboard
+///   sea water density variable: ObsValue/seaWaterDensity
+///   snow depth variable: ObsValue/totalSnowDepth
+///   snow density variable: ObsValue/snowDensity
+///   ice density variable: ObsValue/iceDensity
+///   ice freeboard error standard deviation variable:
+///       ObservedErrorStandardDeviation/seaIceFreeboard
+///   ice density error standard deviation variable:
+///       ObservedErrorStandardDeviation/iceDensity
+///   ice thickness variable: iceThickness
+///   ice thickness systematic error standard deviation variable:
+///       SystematicStandardDeviation/iceThickness
+///   ice thickness random error standard deviation variable:
+///       RandomStandardDeviation/iceThickness
+/// \endcode
+///
+
+class Cal_IceThicknessFromFreeboard : public TransformBase {
+ public:
+  typedef Cal_IceThicknessFromFreeboardParameters Parameters_;
+  Cal_IceThicknessFromFreeboard(
+      const Parameters_ &options, const ObsFilterData &data,
+      const std::shared_ptr<ioda::ObsDataVector<int>> &flags,
+      const std::shared_ptr<ioda::ObsDataVector<float>> &obserr);
+  // Run variable conversion
+  void runTransform(const std::vector<bool> &apply) override;
+
+ private:
+  const std::string classname_ = "Cal_IceThicknessFromFreeboard";
+  Parameters_ options_;
+};
+
+// -----------------------------------------------------------------------------
+
+}  // namespace ufo
+
+#endif  // UFO_VARIABLETRANSFORMS_CAL_ICETHICKNESSFROMFREEBOARD_H_

test/testinput/unit_tests/variabletransforms/CMakeLists.txt
@@ -26,6 +26,16 @@ ufo_add_test( NAME    test_ufo_variabletransforms_logarithm
               LABELS  unit_tests variabletransforms
               WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/../../../ )
 #
+ufo_add_test( NAME    test_ufo_variabletransforms_icefreeboardtothickness
+              TIER    1
+              ECBUILD
+              COMMAND ${CMAKE_BINARY_DIR}/bin/test_ObsFilters.x
+              ARGS    "${CMAKE_CURRENT_SOURCE_DIR}/variabletransforms_icefreeboardtothickness.yaml"
+              MPI     1
+              LIBS    ufo
+              LABELS  unit_tests variabletransforms
+              WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/../../../ )
+#
 if( ${gsw_FOUND} )
     ufo_add_test( NAME    test_ufo_variabletransforms_oceandensity
                 TIER    1

test/testinput/unit_tests/variabletransforms/variabletransforms_icefreeboardtothickness.yaml
@@ -1,0 +1,219 @@
+time window:
+  begin: 2017-12-31T21:00:00Z
+  end: 2030-01-01T03:00:00Z
+
+observations:
+- obs space:
+    name: Ice Thickness from Sea Ice Freeboard with uncertainties
+    obsdatain:
+      engine:
+        type: H5File
+        obsfile: Data/ufo/testinput_tier_1/ice_freeboard_to_thickness_testdata.nc
+    simulated variables:
+     - iceThickness
+    observed variables:
+     - seaIceFreeboard
+     - totalSnowDepth
+     - snowDensity
+     - iceDensity
+    derived variables:
+     - iceThickness
+  obs filters:
+  - filter: Variable Transforms
+    Transform: Calculate iceThickness from seaIceFreeboard
+    ice freeboard variable: ObsValue/seaIceFreeboard
+    sea water density variable: hofx/seaWaterDensity
+    snow depth variable: ObsValue/totalSnowDepth
+    snow density variable: ObsValue/snowDensity
+    ice density variable: ObsValue/iceDensity
+    ice thickness variable: DerivedObsValue/iceThickness
+    calculate error standard deviations: true
+    ice freeboard error standard deviation variable: ObservedErrorStandardDeviation/seaIceFreeboard
+    ice density error standard deviation variable: ObservedErrorStandardDeviation/iceDensity
+    snow depth error standard deviation variable: ObservedErrorStandardDeviation/totalSnowDepth
+    snow density error standard deviation variable: ObservedErrorStandardDeviation/snowDensity
+    ice thickness systematic error standard deviation variable: SystematicErrorStandardDeviation/iceThickness
+    ice thickness random error standard deviation variable: RandomErrorStandardDeviation/iceThickness
+
+  compareVariables:
+    - reference:
+        name: TestReference/iceThickness
+      test:
+        name: DerivedObsValue/iceThickness
+      relTol: 1.0e-6
+    - reference:
+        name: TestReferenceSystematicErrorStandardDeviation/iceThickness
+      test:
+        name: SystematicErrorStandardDeviation/iceThickness
+      relTol: 1.0e-6
+    - reference:
+        name: TestReferenceRandomErrorStandardDeviation/iceThickness
+      test:
+        name: RandomErrorStandardDeviation/iceThickness
+      relTol: 1.0e-6
+    - reference:
+        name: TestReferenceError/iceThickness
+      test:
+        name: EffectiveError/iceThickness
+      relTol: 1.0e-6
+  passedBenchmark: 44  # 50
+                       # - 4 -- 1 missing from each of seaIceFreeboard, iceDensity, snowDensity, and totalSnowDepth
+                       # - 1 -- seaWaterDensity is less than iceDensity for position 5
+                       # - 1 -- ObservedErrorStandardDeviation/iceFreeboard is missing for position 7
+                       # = 44
+- obs space:
+    name: Ice Thickness from Sea Ice Freeboard default parameters with uncertainties
+    obsdatain:
+      engine:
+        type: H5File
+        obsfile: Data/ufo/testinput_tier_1/ice_freeboard_to_thickness_testdata.nc
+    simulated variables:
+     - iceThickness
+    observed variables:
+     - seaIceFreeboard
+     - totalSnowDepth
+     - snowDensity
+     - iceDensity
+    derived variables:
+     - iceThickness
+  obs filters:
+  - filter: Variable Transforms
+    Transform: Calculate iceThickness from seaIceFreeboard
+    sea water density variable: hofx/seaWaterDensity
+    ice thickness variable: DerivedObsValue/iceThickness
+    calculate error standard deviations: true
+
+  compareVariables:
+    - reference:
+        name: TestReference/iceThickness
+      test:
+        name: DerivedObsValue/iceThickness
+      relTol: 1.0e-6
+    - reference:
+        name: TestReferenceSystematicErrorStandardDeviation/iceThickness
+      test:
+        name: SystematicErrorStandardDeviation/iceThickness
+      relTol: 1.0e-6
+    - reference:
+        name: TestReferenceRandomErrorStandardDeviation/iceThickness
+      test:
+        name: RandomErrorStandardDeviation/iceThickness
+      relTol: 1.0e-6
+    - reference:
+        name: TestReferenceError/iceThickness
+      test:
+        name: EffectiveError/iceThickness
+      relTol: 1.0e-6
+  passedBenchmark: 44  # 50
+                       # - 4 -- 1 missing from each of seaIceFreeboard, iceDensity, snowDensity, and totalSnowDepth
+                       # - 1 -- seaWaterDensity is less than iceDensity for position 5
+                       # - 1 -- ObservedErrorStandardDeviation/iceFreeboard is missing for position 7
+                       # = 44
+- obs space:
+    name: Ice Thickness from Sea Ice Freeboard without uncertainties
+    obsdatain:
+      engine:
+        type: H5File
+        obsfile: Data/ufo/testinput_tier_1/ice_freeboard_to_thickness_testdata.nc
+    simulated variables:
+     - iceThickness
+    observed variables:
+     - seaIceFreeboard
+     - totalSnowDepth
+     - snowDensity
+     - iceDensity
+    derived variables:
+     - iceThickness
+  obs filters:
+  - filter: Variable Transforms
+    Transform: Calculate iceThickness from seaIceFreeboard
+    ice freeboard variable: ObsValue/seaIceFreeboard
+    sea water density variable: hofx/seaWaterDensity
+    snow depth variable: ObsValue/totalSnowDepth
+    snow density variable: ObsValue/snowDensity
+    ice density variable: ObsValue/iceDensity
+    ice thickness variable: DerivedObsValue/iceThickness
+    calculate error standard deviations: false
+  - filter: Perform Action
+    action:
+      name: assign error
+      error parameter: 1
+
+  compareVariables:
+    - reference:
+        name: TestReference/iceThickness
+      test:
+        name: DerivedObsValue/iceThickness
+      relTol: 1.0e-6
+  passedBenchmark: 45  # 50
+                       # - 4 -- 1 missing from each of seaIceFreeboard, iceDensity, snowDensity, and totalSnowDepth
+                       # - 1 -- seaWaterDensity is less than iceDensity for position 5
+                       # = 45
+
+- obs space:
+    name: Ice Thickness from Sea Ice Freeboard ObsValue variable exception
+    obsdatain:
+      engine:
+        type: H5File
+        obsfile: Data/ufo/testinput_tier_1/ice_freeboard_to_thickness_testdata.nc
+    simulated variables:
+     - iceThickness
+    observed variables:
+     - seaIceFreeboard
+     - totalSnowDepth
+     - snowDensity
+     - iceDensity
+    derived variables:
+     - iceThickness
+  obs filters:
+  - filter: Variable Transforms
+    Transform: Calculate iceThickness from seaIceFreeboard
+    ice freeboard variable: ObsValue/seaIceFreeboardDoesntExist
+    sea water density variable: hofx/seaWaterDensity
+    snow depth variable: ObsValue/totalSnowDepth
+    snow density variable: ObsValue/snowDensity
+    ice density variable: ObsValue/iceDensity
+    calculate error standard deviations: true
+    ice freeboard error standard deviation variable: ObservedErrorStandardDeviation/seaIceFreeboard
+    ice density error standard deviation variable: ObservedErrorStandardDeviation/iceDensity
+    snow depth error standard deviation variable: ObservedErrorStandardDeviation/totalSnowDepth
+    snow density error standard deviation variable: ObservedErrorStandardDeviation/snowDensity
+    ice thickness variable: DerivedObsValue/iceThickness
+    ice thickness systematic error standard deviation variable: SystematicErrorStandardDeviation/iceThickness
+    ice thickness random error standard deviation variable: RandomErrorStandardDeviation/iceThickness
+  passedBenchmark: 0
+  expectExceptionWithMessage: "Cal_IceThicknessFromFreeboard:: `ObsValue/seaIceFreeboardDoesntExist` must be a variable for this variable transform."
+
+- obs space:
+    name: Ice Thickness from Sea Ice Freeboard error standard deviation variable exception
+    obsdatain:
+      engine:
+        type: H5File
+        obsfile: Data/ufo/testinput_tier_1/ice_freeboard_to_thickness_testdata.nc
+    simulated variables:
+     - iceThickness
+    observed variables:
+     - seaIceFreeboard
+     - totalSnowDepth
+     - snowDensity
+     - iceDensity
+    derived variables:
+     - iceThickness
+  obs filters:
+  - filter: Variable Transforms
+    Transform: Calculate iceThickness from seaIceFreeboard
+    ice freeboard variable: ObsValue/seaIceFreeboard
+    sea water density variable: hofx/seaWaterDensity
+    snow depth variable: ObsValue/totalSnowDepth
+    snow density variable: ObsValue/snowDensity
+    ice density variable: ObsValue/iceDensity
+    calculate error standard deviations: true
+    ice freeboard error standard deviation variable: ObservedErrorStandardDeviation/seaIceFreeboardDoesntExist
+    ice density error standard deviation variable: ObservedErrorStandardDeviation/iceDensity
+    snow depth error standard deviation variable: ObservedErrorStandardDeviation/totalSnowDepth
+    snow density error standard deviation variable: ObservedErrorStandardDeviation/snowDensity
+    ice thickness variable: DerivedObsValue/iceThickness
+    ice thickness systematic error standard deviation variable: SystematicErrorStandardDeviation/iceThickness
+    ice thickness random error standard deviation variable: RandomErrorStandardDeviation/iceThickness
+  passedBenchmark: 0
+  expectExceptionWithMessage: "Cal_IceThicknessFromFreeboard:: `ObservedErrorStandardDeviation/seaIceFreeboardDoesntExist` must be a variable for this variable transform if calculating errors."

commit 99d7ef4ca53ca81635fa283e743eb69b27be690e

Fix coding style (#3813)
* Fix coding style

Broken by #3756.

* Trigger CI

---------

Co-authored-by: mikecooke77 <michael.c.cooke@metoffice.gov.uk>

src/ufo/variabletransforms/Cal_IceThicknessFromFreeboard.cc
@@ -8,6 +8,7 @@
 #include "ufo/variabletransforms/Cal_IceThicknessFromFreeboard.h"
 
 #include <cmath>
+#include <limits>
 
 #include "eckit/log/CodeLocation.h"
 #include "ioda/ObsDataVector.h"
@@ -58,9 +59,11 @@ Cal_IceThicknessFromFreeboard::Cal_IceThicknessFromFreeboard(
 
 void Cal_IceThicknessFromFreeboard::runTransform(
     const std::vector<bool> &apply) {
-  oops::Log::trace() << classname_ << "::runTransform: Calculate Ocean Ice Thickness and errors from ice "
-                        "freeboard, snow, and density observations "
-                     << std::endl;
+  oops::Log::trace()
+    << classname_
+    << "::runTransform: Calculate Ocean Ice Thickness and errors from ice "
+       "freeboard, snow, and density observations "
+    << std::endl;
 
   // dimension
   const size_t nlocs = obsdb_.nlocs();